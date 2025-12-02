4c9b771e7f6ae97487600bf4a05c81dc92a2e5b5
Danila Vaganov <danilavaganov41@gmail.com>
2025-12-02 17:08:16 +0600

    v1.3 bug fix

--- src/components/ui/ChatArea.tsx
+++ src/components/ui/ChatArea.tsx
@@ -7,13 +7,13 @@ import { useMessageStore } from '@/src/store/messagesStore'
 export default function ChatArea() {
 	const { toggleBurger } = useMessageStore()
 	return (
-		<main className='glass bg-white/6 border border-white/12 rounded-xs md:rounded-2xl md:p-5 flex flex-col h-[100vh] p-3 md:w-[75vw] w-[100vw] md:m-auto'>
+		<main className='glass bg-white/6 border border-white/12 rounded-xs md:rounded-2xl md:p-5 flex flex-col h-screen p-3 md:w-[75vw] w-screen md:m-auto'>
 			<div className='flex items-center gap-3 mb-4'>
-				<div className='w-14 h-14 rounded-lg bg-gradient-to-br from-blue-400 to-teal-400 flex items-center justify-center font-bold text-[#041118]'>
+				<div className='w-14 h-14 rounded-lg bg-linear-to-br from-blue-400 to-teal-400 flex items-center justify-center font-bold text-[#041118]'>
 					AI
 				</div>
 				<div>
-					<div className='font-bold'>LiquidMind v1.2</div>
+					<div className='font-bold'>LiquidMind v1.3</div>
 					<div className='text-xs text-white/55'>Чат</div>
 				</div>
 				<button
